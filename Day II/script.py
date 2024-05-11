50c1916daee743e55437c3757c7d75f0554c1be3
cacao-sentinel <binary.theobroma@proton.me>
2024-05-10 23:41:49 -0500

    Learning requests

--- Day II/script.py
+++ Day II/script.py
@@ -1,1 +1,8 @@
-print("Proximamente funcionalidad GitSearch!")
+import requests
+import json
+info = input("Type what repo are you feelin today...")
+response = requests.get(f"https://github.com/search?q={info}&type=repositories")
+
+json_res = response.json()
+
+print(json_res)
