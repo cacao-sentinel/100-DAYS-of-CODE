6c4d58096caf2e997ecdd219f0f9f8b935e6383e
cacao-sentinel <binary.theobroma@proton.me>
2024-05-10 23:43:24 -0500

    Learning requests and json libraries

--- Day II/script.py
+++ Day II/script.py
@@ -5,4 +5,20 @@ response = requests.get(f"https://github.com/search?q={info}&type=repositories")
 
 json_res = response.json()
 
-print(json_res)
+dump = json.dumps(json_res)
+
+data = json.loads(dump)
+
+for result in data['payload']['results']:
+    repo_name = result['repo']['repository']['name']
+    stars = result['followers']
+    author = result['repo']['repository']['owner_login']
+    last_commit_date = result['repo']['repository']['updated_at']
+    language = result['language']
+    
+    print("Nombre del repositorio:", repo_name)
+    print("Cantidad de estrellas:", stars)
+    print("Autor:", author)
+    print("Fecha del último commit:", last_commit_date)
+    print("Lenguaje utilizado:", language)
+    print()
